03403eae27026453b134aec75d9c9c20879ddcb1
Ilya <46647517+Sn0wy-0wl@users.noreply.github.com>
2020-02-16 19:24:22 +0300

    Initial Player and Enemy classes
    Workflow for ilushins

---
+++ Assets/Scripts/Enemies/Enemy.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+
+public abstract class Enemy : MonoBehaviour
+{
+
+
+}
---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    private Rigidbody2D rgbd2D;
+
+    [SerializeField] private float _movingSpeed;
+    [SerializeField] private int _jetPackCapacity = 5;
+    [SerializeField] private int _thrustAmmount = 5;
+
+    private bool _isGrounded;
+
+
+    private void Awake()
+    {
+        rgbd2D = GetComponent<Rigidbody2D>();
+    }
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    public void Jump()
+    {
+
+    }
+
+    public void Move()
+    {
+
+    }
+
+    public void UseThrust()
+    {
+        if (_thrustAmmount == 0)
+        {
+            // TODO: Logic for no trust situation
+            return;
+        }
+        
+        _thrustAmmount--;
+
+    }
+}
