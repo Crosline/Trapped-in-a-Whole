f94c9e26058e018c5417f64258214dd10bce9e8d
BuildTools <unconfigured@null.spigotmc.org>
2020-02-16 19:18:42 +0300

    PauseMenu

---
+++ Assets/Scripts/Menu/PauseMenu.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour {
+
+    public static bool isPaused;
+
+    public GameObject pauseMenu;
+
+    void Start() {
+
+    }
+
+    void Update() {
+
+        if (Input.GetButtonDown("Cancel")) {
+            if (isPaused)
+                Resume();
+            else
+                Pause();
+        }
+
+    }
+
+    #region Pause Methods
+
+    public void Resume() {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+    }
+    private void Pause() {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
+
+
+    #endregion
+
+    #region Scene Methods
+    public void ReturnMenu() {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void ExitGame() {
+        Application.Quit();
+    }
+
+
+    #endregion
+
+}
