582f1fba9f0895e7c5ff9609bcc90d03ef29a7f4
Ilya <46647517+Sn0wy-0wl@users.noreply.github.com>
2020-02-17 14:06:32 +0300

    Player v0.1

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -5,18 +5,24 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
 
-    private Rigidbody2D rgbd2D;
+    private Rigidbody2D _rgbd2D;
+    private BoxCollider2D _collider;
 
     [SerializeField] private float _movingSpeed;
-    [SerializeField] private int _jetPackCapacity = 5;
-    [SerializeField] private int _thrustAmmount = 5;
+    [SerializeField] private float _airMovingSpeed;
+    [SerializeField] private float _jetPackCapacity;
+    [SerializeField] private float _thrustAmmount;
 
-    private bool _isGrounded;
+    [SerializeField] private float _jetPackRestoringSpeed;
+    [SerializeField] private float _dashTimeout;
 
+    private bool _isGrounded = true;
+    private bool _isJumped = false;
 
     private void Awake()
     {
-        rgbd2D = GetComponent<Rigidbody2D>();
+        _rgbd2D = GetComponent<Rigidbody2D>();
+        _collider = GetComponent<BoxCollider2D>();
     }
 
     private void Start()
@@ -26,28 +32,49 @@ public class Player : MonoBehaviour
 
     private void Update()
     {
-        
+        Move();
     }
 
-    public void Jump()
+    private void Jump()
     {
-
+        _isGrounded = false;
+        _isJumped = true;
+        _rgbd2D.AddForce(Vector3.up * Time.deltaTime / _rgbd2D.gravityScale * 10000); // / _rgbd2D.gravityScale 
+        Debug.Log("JUMP");
     }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("Collided!");
+    }
+
 
     public void Move()
     {
+        if (Input.GetButtonDown("Jump") && !_isJumped)
+            Jump();
 
+        else if (Input.GetButton("Jump") && _isJumped)
+            UseThrust();
+
+        if (Input.GetAxis("Horizontal") != 0)
+            transform.position = (transform.position + new Vector3(_movingSpeed * Time.deltaTime * Input.GetAxis("Horizontal"),0));
     }
 
-    public void UseThrust()
+    private void UseThrust()
     {
-        if (_thrustAmmount == 0)
+        if (_thrustAmmount <= 0)
         {
             // TODO: Logic for no trust situation
+            _thrustAmmount = 0;
             return;
         }
-        
-        _thrustAmmount--;
+        _thrustAmmount -= Time.deltaTime;
+        _rgbd2D.AddForce(Vector3.up * Time.deltaTime / _rgbd2D.gravityScale * 1000);
+    }
+
+    private void Dash()
+    {
 
     }
 }
