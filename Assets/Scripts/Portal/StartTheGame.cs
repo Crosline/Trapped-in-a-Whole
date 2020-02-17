3fe3973db924f55d4adeabdce3dfdadbbe4ecc50
BuildTools <unconfigured@null.spigotmc.org>
2020-02-18 00:13:29 +0300

    Quickfix StartGame.cs

--- Assets/Scripts/Portal/StartTheGame.cs
+++ Assets/Scripts/Portal/StartTheGame.cs
@@ -1,9 +1,13 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class StartGame : MonoBehaviour {
+public class StartTheGame : MonoBehaviour {
+
+
 	// Start is called before the first frame update
 	void Start() {
 		SceneManager.LoadScene("level1-3");
 	}
+
+
 }
