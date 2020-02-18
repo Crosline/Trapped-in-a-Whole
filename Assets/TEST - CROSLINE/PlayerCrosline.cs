6a3ce462676ad545df48523f999e00ac7471505a
BuildTools <unconfigured@null.spigotmc.org>
2020-02-18 12:38:26 +0300

    Playtest / Test Scene (Crosline)

--- Assets/TEST - CROSLINE/PlayerCrosline.cs
+++ Assets/TEST - CROSLINE/PlayerCrosline.cs
@@ -17,9 +17,13 @@ public class PlayerCrosline : MonoBehaviour {
     public float fallMultiplier = 2.5f;
     public float lowKeyMultiplier = 1f;
 
+
+    public Animator anime;
+
     [Header("Movement Options")]
 
     private bool isDashing;
+    public float dashForce;
 
     [Header("Player Options")]
     public float health = 3;
@@ -40,7 +44,6 @@ public class PlayerCrosline : MonoBehaviour {
     public PhysicsMaterial2D withFriction;
 
     void Start() {
-        //anime = GetComponent<Animator>();
         jetpackTimeCounter = jetPackTime;
     }
 
@@ -89,13 +92,20 @@ public class PlayerCrosline : MonoBehaviour {
         }
     }
 
-    private void Dash() {
 
+    private void Dash() {
+        if(isDashing && jetpackTimeCounter > jetPackTime * 2 / 5) {
+            rb.velocity += Vector2.right * rb.velocity.x * dashForce;
+            jetpackTimeCounter -= jetPackTime * 2 / 5;
+            jetpackTimeCounter = Mathf.Clamp(jetpackTimeCounter, 0, 100);
+            isDashing = false;
+        }
     }
+
 
     private void Jump() {
 
-        /*if (rb.velocity.y < 0)
+        if (rb.velocity.y < 0)
             anime.SetBool("Fall", true); //fall animation here*/
 
 
@@ -110,34 +120,35 @@ public class PlayerCrosline : MonoBehaviour {
 
 
         if (jump) {
+            anime.SetBool("Jump", true);
             jump = false;
             rb.velocity = Vector2.up * jumpForce + Vector2.right * rb.velocity.x;
             isJumping = true;
         }
 
         if (Input.GetButton("Jump") && !isGrounded && jetpackTimeCounter > 0) {
+            anime.SetBool("Thrust", true);
             if (rb.velocity.y < 0) {
                 rb.velocity = Vector2.up * jetPackForce * Time.fixedDeltaTime + Vector2.right * rb.velocity.x;
             }
             rb.velocity += Vector2.up * jetPackForce * Time.fixedDeltaTime + Vector2.right * rb.velocity.x;
             jetpackTimeCounter -= Time.fixedDeltaTime * jetPackFuelRate;
-        }
+        } else if (jetpackTimeCounter < 0)
+            anime.SetBool("Thrust", false);
 
         if (isGrounded) {
 
             if (jetpackTimeCounter < jetPackTime) {
                 Debug.Log("Hello");
                 isJumping = false;
+                anime.SetBool("Jump", false);
                 jetpackTimeCounter += Time.fixedDeltaTime * jetPackFuelRate;
             }
 
-
-        }
-        /*anime.SetBool("Jump", false);
-        anime.SetBool("Fall", false);
-        anime.SetBool("Ground", true);
-    } else
-        anime.SetBool("Ground", false);*/
+            anime.SetBool("Ground", true);
+            anime.SetBool("Fall", false);
+        } else
+            anime.SetBool("Ground", false);
 
     }
 
@@ -162,14 +173,16 @@ public class PlayerCrosline : MonoBehaviour {
 
         moveHorizontal = Input.GetAxis("Horizontal") * playerSpeed;
 
-
+        
         if (moveHorizontal != 0) {
 
             rb.sharedMaterial = noFriction;
             // animation speed 1
+            anime.SetFloat("Speed", 1);
 
         } else {
             //animation speed 0
+            anime.SetFloat("Speed", 0);
             rb.sharedMaterial = withFriction;
         }
     }
@@ -178,6 +191,7 @@ public class PlayerCrosline : MonoBehaviour {
     private void DashInput() {
         if (Input.GetButtonDown("Fire1")) {
             isDashing = true;
+            anime.SetTrigger("Dash");
         }
 
     }
---
+++ Assets/TEST - CROSLINE/Resources/Player/Fall/ReadMe.txt
@@ -0,0 +1,2 @@
+Land-1 and Land-2 from falling are different from Fall are differents from the ones in Jump
+Use these ones as defaults and the others exclusively for jumping.
