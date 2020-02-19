68d2b9dbdd3300223f59ad383f0d5358ca0e3733
Ilya <46647517+Sn0wy-0wl@users.noreply.github.com>
2020-02-19 14:08:49 +0300

    Serialization

--- Assets/Scripts/Managers/DataManger.cs
+++ Assets/Scripts/Managers/DataManger.cs
@@ -1,25 +1,32 @@
 ﻿using System.IO;
-
+using System.Runtime.Serialization.Formatters.Binary;
+using UnityEngine;
 
 public class DataManger
 {
 
-    public T LoadData<T>(string dataPath)
+    public static void SaveData<T>(T playerData, string fileName)
     {
-        Stream stream =
-        BinaryWriter writer = new BinaryWriter();
-
-
-
+        BinaryFormatter formatter = new BinaryFormatter();
+        fileName = Application.persistentDataPath + "/" + fileName;
+        FileStream stream = new FileStream(fileName, FileMode.Create);
+        formatter.Serialize(stream, playerData);
+        stream.Close();
     }
 
-    public void SaveData<T>(string dataPath) where T : class
+    public static T LoadData<T>(string fileName) where T : class
     {
-
-
-
+        fileName = Application.persistentDataPath + "/" + fileName;
+        if (File.Exists(fileName))
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            using (var stream = File.OpenRead(fileName))
+                return formatter.Deserialize(stream) as T;
+        }
+        else
+        {
+            Debug.LogError("File not found...");
+            return null;
+        }
     }
-
-
-
 }
