27b2f4e3cc6870787a17a9e866e56c2adeae38e8
BuildTools <unconfigured@null.spigotmc.org>
2020-02-20 17:12:22 +0300

    Shards connected & painted

--- Assets/Scripts/PickUp/CrystalShard.cs
+++ Assets/Scripts/PickUp/CrystalShard.cs
@@ -1,29 +1,45 @@
 ﻿using UnityEngine;
 
-public class CrystalShard : MonoBehaviour, IPickUp
-{
-    private Collider2D _collider;
+public class CrystalShard : MonoBehaviour, IPickUp {
+
+    private Collider2D _collider2D;
     private Animator _animator;
     private bool isPickedUp = false;
+    public LayerMask layerMask;
 
     private void Awake()
     {
-        _collider = GetComponent<Collider2D>();
+        _collider2D = GetComponent<Collider2D>();
         _animator = GetComponent<Animator>();
     }
 
     public void PickUp()
     {
         isPickedUp = true;
-        _animator.Play("Shard Die Animation Name");
+        _animator.SetTrigger("Collect");
+        _collider2D.enabled = false;
         PickUpEffect();
     }
 
     public void PickUpEffect() => Player.CurrentLevel.ShardsCollected++;
 
+
+    void Update() {
+
+        if (isPickedUp) {
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, 0.4f, layerMask);
+            Debug.DrawRay(transform.position, Vector2.down * (.4f), Color.red);
+            if (hit.collider.tag != "DefaultTile")
+                transform.position = new Vector2(transform.position.x, transform.position.y - 1 * Time.deltaTime);
+        }
+
+    }
+
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Player" && !isPickedUp)
             PickUp();
+
     }
 }
