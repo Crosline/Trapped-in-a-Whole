b9686f32241026d9de39cc459dddffc4273861cd
BuildTools <b21727025@cs.hacettepe.edu.tr>
2020-02-23 04:43:23 +0300

    Last 3

---
+++ Assets/Cyclops.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cyclops : Walker
+{
+
+    private bool isSpitting;
+    public GameObject spit;
+
+    private void Update() {
+        if (CheckForPlayer())
+            StartCoroutine(Spit());
+    }
+
+    IEnumerator Spit() {
+        if (isSpitting)
+            yield break;
+        isSpitting = true;
+        _animator.SetTrigger("isSpotted");
+        yield return new WaitForSeconds(0.3f);
+        Instantiate(spit, transform.position, Quaternion.identity);
+
+        _animator.ResetTrigger("isSpotted");
+        isSpitting = false;
+        yield break;
+    }
+
+
+}
